71e348c946977ada36fb6ca9baa9d49abdf2a7d5
Ethan-Hyde <elw.hyde@gmail.com>
2016-10-17 16:04:16 +1300

    Add files via upload

---
+++ 2016002627 Lab 1/ConsoleApplication12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello. Please state your name.");
+
+            string name = Console.ReadLine();
+           
+            Console.WriteLine("Welcome {0} to D520 Programming 1.", name);
+
+            Thread.Sleep(2000);
+
+            //holding console screen 
+            Console.WriteLine("press enter to exit console application");
+            Console.ReadLine();
+        }
+    }
+}
